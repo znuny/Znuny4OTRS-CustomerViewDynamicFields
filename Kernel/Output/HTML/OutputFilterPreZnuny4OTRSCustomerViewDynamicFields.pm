411845575fe3b40acb0fe5e97a672c4b2b7df775
Denny Bresch <db@znuny.com>
2018-11-01 23:09:19 +0100

    Applied CodePolicy.

--- Kernel/Output/HTML/OutputFilterPreZnuny4OTRSCustomerViewDynamicFields.pm
+++ Kernel/Output/HTML/OutputFilterPreZnuny4OTRSCustomerViewDynamicFields.pm
@@ -1,6 +1,9 @@
 # --
-# Kernel/Output/HTML/OutputFilterPreZnuny4OTRSCustomerViewDynamicFields.pm - adds the DynamicField block to the AgentTicketCustomer template
-# Copyright (C) 2014 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2012-2018 Znuny GmbH, http://znuny.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
 package Kernel::Output::HTML::OutputFilterPreZnuny4OTRSCustomerViewDynamicFields;
@@ -9,6 +12,7 @@ use strict;
 use warnings;
 
 use Kernel::System::VariableCheck qw(:all);
+
 sub new {
     my ( $Type, %Param ) = @_;
 
@@ -37,7 +41,8 @@ sub Run {
 HTML
 
     # manipulate HTML content
-    ${ $Param{Data} } =~ s{(<label\s(class="Mandatory"\s|)for="CustomerID".+?div\sclass="Clear"></div>)}{$1$DynamicFieldHTMLBlock}xms;
+    ${ $Param{Data} }
+        =~ s{(<label\s(class="Mandatory"\s|)for="CustomerID".+?div\sclass="Clear"></div>)}{$1$DynamicFieldHTMLBlock}xms;
 
     return 1;
 }
--- README.md
+++ README.md
@@ -30,4 +30,3 @@ Enjoy!
  Your Znuny Team!
 
  [http://znuny.com](http://znuny.com)
-
