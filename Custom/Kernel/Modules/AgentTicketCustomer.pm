1a093a972d1286073e32b6493435bdbdff97e024
Denny Korsukéwitz <dk@znuny.com>
2021-01-20 12:55:46 +0100

    Happy new year. Tidied code.

--- Custom/Kernel/Modules/AgentTicketCustomer.pm
+++ Custom/Kernel/Modules/AgentTicketCustomer.pm
@@ -1,6 +1,6 @@
 # --
-# Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2012-2020 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2021 OTRS AG, https://otrs.com/
+# Copyright (C) 2012-2021 Znuny GmbH, http://znuny.com/
 # --
 # $origin: otrs - 0000000000000000000000000000000000000000 - Kernel/Modules/AgentTicketCustomer.pm
 # --
